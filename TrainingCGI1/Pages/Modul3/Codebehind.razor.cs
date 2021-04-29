7f972d811cf2cee28cb6a4329a64a5a3bb4df609
HannesPreishuber <hannesp@ppedv.de>
2021-04-29 12:03:07 +0200

    Codebehind Blazor cs

---
+++ TrainingCGI1/Pages/Modul3/Codebehind.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingCGI1.Pages.Modul3
+{
+    public partial class Codebehind
+    {
+    }
+}
