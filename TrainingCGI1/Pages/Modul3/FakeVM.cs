cd891eb037e66d20478e2846bbf3ad371fbf5303
HannesPreishuber <hannesp@ppedv.de>
2021-04-29 14:23:41 +0200

    Mini ViewModel

---
+++ TrainingCGI1/Pages/Modul3/FakeVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingCGI1.Pages.Modul3
+{
+    public class FakeVM
+    {
+        public List<string> Tage { get; set; }
+       public void Init()
+        {
+            Tage = new List<string>();
+            for (int i = 0; i < 7; i++)
+            {
+                Tage.Add(DateTime.Now.AddDays(i).ToString("dddd"));
+            }
+
+        }
+    }
+}
