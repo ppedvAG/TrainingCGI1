32d64516585215507224b781a799e50aee7a3895
HannesPreishuber <hannesp@ppedv.de>
2021-04-30 16:12:55 +0200

    kleine JS Schweinerein

---
+++ TrainingCGI1/Pages/modul8/jsintro.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TrainingCGI1.Pages.modul8
+{
+    public class jsintroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
