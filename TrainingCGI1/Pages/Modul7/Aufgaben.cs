6ae7dd781ecf580128303220db1e60ef28c898ca
HannesPreishuber <hannesp@ppedv.de>
2021-04-30 14:54:21 +0200

    Template Liste mit variablen Datentyp

---
+++ TrainingCGI1/Pages/Modul7/Aufgaben.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingCGI1.Pages.Modul7
+{
+    public class Aufgaben
+    {
+        public int ID { get; set; }
+        public string Text { get; set; }
+    }
+}
