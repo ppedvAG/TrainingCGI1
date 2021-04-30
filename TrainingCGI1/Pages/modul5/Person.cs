3d50ad3a6da14000ab7c3a0854b016cecc5ff1fc
HannesPreishuber <hannesp@ppedv.de>
2021-04-30 09:49:48 +0200

    Model mit DataAnnotations

---
+++ TrainingCGI1/Pages/modul5/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingCGI1.Pages.modul5
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "Da amuss was rein")]
+        public int Gross { get; set; }
+        [Required(ErrorMessage ="Da amuss was rein")]
+        [Range(minimum:140,maximum:195,ErrorMessage ="keine Achterbahn")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Da amuss was rein")]
+        [StringLength(maximumLength:20,ErrorMessage ="zu lange")]
+        public DateTime GebDat { get; set; }
+    }
+}
