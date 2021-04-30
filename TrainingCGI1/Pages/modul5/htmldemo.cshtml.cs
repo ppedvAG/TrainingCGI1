167857118f964fea2a226f40db86baf8f2249cbd
HannesPreishuber <hannesp@ppedv.de>
2021-04-30 09:44:56 +0200

    HTML Eingabe Demo

---
+++ TrainingCGI1/Pages/modul5/htmldemo.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TrainingCGI1.Pages.modul5
+{
+    public class htmldemoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
