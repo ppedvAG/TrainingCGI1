3917bacff149d1920e099370fc677b0453a8e0c4
HannesPreishuber <hannesp@ppedv.de>
2021-04-30 10:27:37 +0200

    CSS FieldClassProvider für Bootstrap Fehlermeldungen

---
+++ TrainingCGI1/Pages/modul5/CSSFormClassProvider.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingCGI1.Pages.modul5
+{
+    public class CSSFormClassProvider : FieldCssClassProvider
+    {
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            var IsValid = !editContext.GetValidationMessages(fieldIdentifier).Any();
+            return IsValid ? "is-valid" : "is-invalid";
+        
+        }
+    }
+}
